457a1d586728df86158c3dba1b5856d4aa43fa52
Silas Aurelius <silasaureliusleone@gmail.com>
2024-10-26 18:32:33 -0400

    second commit

--- customer-service-data-handling.py
+++ customer-service-data-handling.py
@@ -8,19 +8,11 @@ Update the status of an existing ticket.
 Display all tickets or filter by status.
 Initialize with some sample tickets and include functionality for additional ticket entry.
 """
-ticket_number = 002.0
+ticket_number = 2
 service_tickets = {
     "Ticket001": {"Customer": "Alice", "Issue": "Login problem", "Status": "open"},
     "Ticket002": {"Customer": "Bob", "Issue": "Payment issue", "Status": "closed"}
-}
-
-def add_new_serive_ticket(service_tickets):
-    new_customer = input("Enter customer name: ")
-    new_issue = input("Enter issue: ")
-    new_status = input("Enter status (open/closed): ")
-    
-    
-    
+} 
 
 while True:
     print("===MENU===")
@@ -28,10 +20,30 @@ while True:
     print("2. Update Status of ticket.")
     print("3. Display all tickets.")
     print("4. Display all tickets by status.")
+    print("5. Quit")
     
     user_input = int(input("Enter numeric selection: "))
     if user_input == 1:
-        add_new_serive_ticket()
+        new_customer = input("Enter customer name: ").title()
+        new_issue = input("Enter issue: ").lower()
+        new_status = input("Enter status (open/closed): ").lower()
         ticket_number += 1
-        service_tickets[(f"Ticket{ticket_number}")][new]
-    pass
+        service_tickets[f"Ticket{ticket_number}"] = {"Customer": new_customer, "Issue": new_issue, "Status": new_status}
+        print(service_tickets)
+    elif user_input == 2:
+        update_ticket = input("Enter ticket number i.e. 'Ticket000' etc.: ")
+        update_status = input("Enter status (open/closed): ").lower()
+        if update_ticket and new_customer not in ticket_number:
+            print("Ticket number and/or customer name not found.")
+        else:
+            updated_service_ticket = {f"Ticket{ticket_number}": {"Customer": {new_customer}, "Issue": {new_issue}, "Status": {update_status}}} 
+            service_tickets.update(update_status)
+    elif user_input == 3:
+        for display in service_tickets:
+            print(f"\n{display}")
+    elif user_input == 4:
+        print(service_tickets.sort())
+    else:
+        print("Thank you for using the Customer Service Database.\nGoodbye")
+        break
+        
